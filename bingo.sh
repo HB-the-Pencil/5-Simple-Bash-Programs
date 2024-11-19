b3d843658434d3132504cd2a04a08a0263e943b6
Henry Barcalow <118207101+HB-the-Pencil@users.noreply.github.com>
2024-11-19 08:32:04 -0500

    Update bingo.sh

--- bingo.sh
+++ bingo.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
-# Bingo Number Generator
+# Bingo Number Generator.
+# Edited from the freeCodeCamp version to improve the formatting.
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
 NUMBER=$(( RANDOM % 75 + 1 ))
-TEXT="The next number is, "
+TEXT="The next number is "
 
 if (( NUMBER <= 15 ))
 then
-  echo $TEXT B:$NUMBER
-elif [[ $NUMBER -le 30 ]]
+  echo $TEXT "B:" $NUMBER
+elif (( $NUMBER <= 30 ))
 then
-  echo $TEXT I:$NUMBER
-elif (( NUMBER < 46 ))
+  echo $TEXT "I:" $NUMBER
+elif (( NUMBER <= 45 ))
 then
-  echo $TEXT N:$NUMBER
-elif [[ $NUMBER -lt 61 ]]
+  echo $TEXT "N:" $NUMBER
+elif (( $NUMBER <= 60 ))
 then
-  echo $TEXT G:$NUMBER
+  echo $TEXT "G:" $NUMBER
 else
-  echo $TEXT O:$NUMBER
+  echo $TEXT "O:" $NUMBER
 fi
